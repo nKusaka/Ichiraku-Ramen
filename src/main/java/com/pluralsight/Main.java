518ea1a5e16c2f8eaf1df62658966a0960bf5dcd
Nik <nikkusaka@gmail.com>
2025-11-06 15:58:51 -0800

    -Created a new myutilities package and added a loading bar simulation

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,6 +1,9 @@
 package com.pluralsight;
+import com.pluralsight.*;
+import com.myutilities.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+    public static void main(String[] args) throws Exception {
+        Utilities.loadingBar("Hi");
     }
 }
