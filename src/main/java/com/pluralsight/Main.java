0002cfef438c73e9ade84897245c32834603e253
Nik <nikkusaka@gmail.com>
2025-11-12 18:26:41 -0800

    -Actually started now

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -8,6 +8,6 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) {
         UserInterface ui = new UserInterface();
-        ui.welcomeScreen();
+        ui.init();
     }
 }
